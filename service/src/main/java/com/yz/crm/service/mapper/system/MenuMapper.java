c093e82dde5dfcc08e98d8a87467d4419da0cfc9
195144146 <195144146@qq.com>
2018-05-21 15:37:16 +0800

    添加资源增删改接口

--- service/src/main/java/com/yz/crm/service/mapper/system/MenuMapper.java
+++ service/src/main/java/com/yz/crm/service/mapper/system/MenuMapper.java
@@ -18,4 +18,28 @@ public interface MenuMapper {
      */
     List<Resources> selectResourcesTree(@Param("id") Integer id);
 
+    /**
+     * 通过资源地址和id查询资源数
+     * @param param
+     * @return
+     */
+    Integer selectResourcesCountByPath(Resources param);
+
+    /**
+     * 新增资源
+     * @param param
+     */
+    void insertResources(Resources param);
+
+    /**
+     * 通过id更新资源项
+     * @param param
+     */
+    void updateResourcesById(Resources param);
+
+    /**
+     * 通过id删除资源
+     * @param param
+     */
+    void deleteResourcesById(Resources param);
 }
--- service/src/main/java/com/yz/crm/service/service/system/IMenuService.java
+++ service/src/main/java/com/yz/crm/service/service/system/IMenuService.java
@@ -17,4 +17,22 @@ public interface IMenuService {
      * @return
      */
     List<Resources> selectResourcesTree(Integer pid);
+
+    /**
+     * 新增资源
+     * @param param
+     */
+    void insertResources(Resources param) throws Exception;
+
+    /**
+     * 通过id更新资源
+     * @param param
+     */
+    void updateResourcesById(Resources param) throws Exception;
+
+    /**
+     * 通过id删除资源
+     * @param param
+     */
+    void deleteResourcesById(Resources param);
 }
